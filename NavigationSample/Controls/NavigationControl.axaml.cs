fcbaee82de1359089520121f5a876413656c6fed
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2020-11-30 08:48:29 +0000

    Move to controls

---
+++ NavigationSample/Controls/NavigationControl.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls.Primitives;
+
+namespace NavigationSample.Controls
+{
+    public class NavigationControl : TemplatedControl
+    {
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+        }
+    }
+}
